f166c780523a77bce48e8c86b30d5241b4332c9b
Siliciumo2 <147613900+siliciumo2@users.noreply.github.com>
2023-11-17 11:22:28 +0300

    Add files via upload

---
+++ Lab8.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите значения для x, y");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32((Console.ReadLine()));
+try
+{
+    int z = x / y;
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Исключение");
+}
+try
+{
+    int z = x / y;
+    if (x == 0)
+    {
+        throw new Exception("x=0");
+    }
+    else
+    {
+        Console.WriteLine($"Ответ{z}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
